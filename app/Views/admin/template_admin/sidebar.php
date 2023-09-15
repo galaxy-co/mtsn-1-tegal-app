a8940fb26c0b058f6ecc49cd894c96e3d209ff34
maulal ardi atqo <maulalardiatqo123@gmail.com>
2023-09-16 00:55:08 +0700

    pushan

--- app/Views/admin/template_admin/sidebar.php
+++ app/Views/admin/template_admin/sidebar.php
@@ -10,30 +10,12 @@
 							<a data-toggle="collapse" href="#collapseExample" aria-expanded="true">
 								<span>
 									<span class="user-level">Administrator</span>
-									<span class="caret"></span>
+									
 								</span>
 							</a>
 							<div class="clearfix"></div>
 
-							<div class="collapse in" id="collapseExample">
-								<ul class="nav">
-									<li>
-										<a href="#profile">
-											<span class="link-collapse">My Profile</span>
-										</a>
-									</li>
-									<li>
-										<a href="#edit">
-											<span class="link-collapse">Edit Profile</span>
-										</a>
-									</li>
-									<li>
-										<a href="#settings">
-											<span class="link-collapse">Settings</span>
-										</a>
-									</li>
-								</ul>
-							</div>
+							
 						</div>
 					</div>
 					<ul class="nav nav-success">
