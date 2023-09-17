039d148fd2ce828882564a3f0f3ef73c34ee0862
rizalazky <rizalazky26@gmail.com>
2023-09-17 13:38:41 +0700

    update elemen UI

--- app/Views/p5/elemen.php
+++ app/Views/p5/elemen.php
@@ -22,70 +22,18 @@
                 </li>
             </ul>
             <div class="tab-content" id="myTabContent">
-                <div class="card mt-10">
-                  <div class="card-header">
-                    <h5 class="card-title">
-                      FORM ELEMEN
-                    </h5>
-                  </div>
-                  <form action="<?= base_url('admin/p5/store/elemen')?>" id='form-elemen' method="post">
-                    <input type="hidden" name="id_element">
-                    <div class="card-body">
-                      <div class="row">
-                        <div class="col-md-6">
-                          <div class="form-group">
-                            <label for="dimensi">DIMENSI</label>
-                            <select name="dimensi" id="" class='form-control'>
-                              <option value=""></option>
-                              <?php foreach($dimensi as $dimen) : ?>
-                                <option value="<?php echo $dimen['id_dimensi']?>"> <?= $dimen['kode_dimensi'].' - '.$dimen['dimensi']?></option>
-                              <?php endforeach ?>
-                            </select>
-                          </div>
-                          <div class="form-group">
-                            <label for="dimensi">Kode</label>
-                            <input type="text" name="kode_element" class='form-control'>
-                          </div>
-                          <div class="form-group">
-                            <label for="dimensi">Elemen</label>
-                            <textarea name="desc" id="desc" class='form-control' cols="30" rows="3"></textarea>
-                          </div>
-                          
-                        </div>
-                        <div class="col-md-6">
-                          <?php if(count($elemen) > 0): ?>
-                          <div class="form-group">
-                            <label for="dimensi">Parent Elemen</label>
-                            <select name="id_parent_element" id="" class='form-control'>
-                              <option value="null"></option>
-                              <?php foreach($elemen as $el): ?>
-                                <?php if($el['id_parent_element'] == null): ?>
-                                  <option value="<?= $el['id_element']?>"><?php echo $el['kode_element'].' '. $el['desc'] ?></option>
-                                <?php endif ?>
-                              <?php endforeach ?>
-                            </select>
-                          </div>
-                          <?php endif ?>
-                          <div class="form-group d-none sub-element-input">
-                            <label for="dimensi">Nilai Rahmatan Lil Alamin</label>
-                            <input type="text" name="nilai_rahmatan_lil_alamin" class='form-control'>
-                          </div>
-                          <div class="form-group d-none sub-element-input">
-                            <label for="dimensi">Sub Nilai</label>
-                            <input type="text" name="sub_nilai" class='form-control'>
-                          </div>
-
-                        </div>
-                      </div>
-                    </div>
-                    <div class="card-footer">
-                      <button type="submit" class='btn btn-info btn-submit'>SAVE</button>
-                    </div>
-                  </form>
-                </div>
+                
                 <div class="card">
-                  <div class="card-header">
-                    <div class="card-title">ELEMEN</div>
+                  <div class="card-header row">
+                    <div class="col-md-10">
+                      <div class="card-title">ELEMEN</div>
+
+                    </div>
+                    <div class="col-md-2">
+                      <button type="button" class="btn btn-primary action-element" data-toggle="modal"  data-target="#modal-element">
+                        ADD ELEMEN
+                      </button>
+                    </div>
                   </div> 
                   <div class="card-body">
                     <div class="table-responsive col-md-12">
@@ -105,7 +53,7 @@
                                 <td><?= $el['desc'] ?></td>
                                 <td>
                                     <button 
-                                      class='btn btn-info btn-sm btn-edit-element'
+                                      class='btn btn-info btn-sm btn-edit-element action-element'
                                       data-dimensi="<?= $el['id_dimensi']?>"
                                       data-idelement="<?= $el['id_element']?>"
                                       data-kodeelement="<?= $el['kode_element']?>"
@@ -113,6 +61,7 @@
                                       data-nilairahmatanlilalamin=""
                                       data-subnilai=""
                                       style="text-decoration:none"
+                                      data-toggle="modal" data-target="#modal-element"
                                       >
                                       <i class="icon-note"></i> Edit
                                     </button>  
@@ -128,8 +77,16 @@
                   </div> 
                 </div>
                 <div class="card">
-                  <div class="card-header">
-                    <div class="card-title">SUB ELEMEN</div>
+                  <div class="card-header row">
+                    <div class="col-md-10">
+                      <div class="card-title">SUB ELEMEN</div>
+
+                    </div>
+                    <div class="col-md-2">
+                      <button type="button" class="btn btn-primary action-sub-element" data-toggle="modal" data-target="#modal-element">
+                        ADD SUB ELEMEN
+                      </button>
+                    </div>
                   </div> 
                   <div class="card-body">
                     <div class="table-responsive col-md-12">
@@ -157,7 +114,7 @@
                                 <td><?= $el['sub_nilai'] ?></td>
                                 <td>
                                     <button 
-                                      class='btn btn-info btn-sm btn-edit-element'
+                                      class='btn btn-info btn-sm btn-edit-element action-sub-element'
                                       data-dimensi="<?= $el['id_dimensi']?>"
                                       data-idelement="<?= $el['id_element']?>"
                                       data-kodeelement="<?= $el['kode_element']?>"
@@ -166,6 +123,8 @@
                                       data-nilairahmatanlilalamin="<?= $el['nilai_rahmatan_lil_alamin'] ?>"
                                       data-subnilai="<?=$el['sub_nilai']?> "
                                       style="text-decoration:none"
+                                      data-toggle="modal" 
+                                      data-target="#modal-element"
                                       >
                                       <i class="icon-note"></i> Edit
                                     </button>  
@@ -182,64 +141,18 @@
                   </div> 
                 </div>
 
-                <!-- CAPAIAN -->
-                <div class="card mt-10">
-                  <div class="card-header">
-                    <h5 class="card-title">
-                      FORM CAPAIAN
-                    </h5>
-                  </div>
-                  <form action="<?= base_url('admin/p5/store/capaian')?>" method="post">
-                    <input type="hidden" name="id_capaian">
-                    <div class="card-body">
-                      <div class="row">
-                        <div class="col-md-6">
-                          <div class="form-group">
-                            <label for="dimensi">Kode Capaian</label>
-                            <input type="text" readonly name="kode_capaian" class='form-control'>
-                          </div>
-                          
-                          
-                          <div class="form-group">
-                            <label for="dimensi">Elemen</label>
-                            <select name="id_parent_element" id="parent-element-capaian" class='form-control'>
-                              <option value="null"></option>
-                              <?php foreach($elemen as $el): ?>
-                                <?php if($el['id_parent_element']): ?>
-                                  <option data-kodeelement='<?php echo $el['kode_element'] ?>' value="<?= $el['id_element']?>"><?php echo $el['kode_element'].' '. $el['desc'] ?></option>
-                                <?php endif ?>
-                              <?php endforeach ?>
-                            </select>
-                          </div>
-                          <div class="form-group">
-                            <label for="dimensi">Capaian</label>
-                            <textarea name="desc" id="" class='form-control' cols="30" rows="10">
-                              
-                            </textarea>
-                          </div>
-                        </div>
-                        <div class="col-md-6">
-                          
-                          <div class="form-group">
-                            <label for="dimensi">Nilai Rahmatan Lil Alamin</label>
-                            <input type="text" name="nilai_rahmatan_lil_alamin" class='form-control'>
-                          </div>
-                          <div class="form-group">
-                            <label for="dimensi">Sub Nilai</label>
-                            <input type="text" name="sub_nilai" class='form-control'>
-                          </div>
-
-                        </div>
-                      </div>
-                    </div>
-                    <div class="card-footer">
-                      <button type="submit" class='btn btn-info btn-submit'>SAVE</button>
-                    </div>
-                  </form>
-                </div>
+                
                 <div class="card">
-                  <div class="card-header">
-                    <div class="card-title">CAPAIAN</div>
+                  <div class="card-header row">
+                  <div class="col-md-10">
+                      <div class="card-title">CAPAIAN</div>
+
+                    </div>
+                    <div class="col-md-2">
+                      <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modal-capaian">
+                        ADD CAPAIAN
+                      </button>
+                    </div>
                   </div> 
                   <div class="card-body">
                     <div class="table-responsive col-md-12">
@@ -272,6 +185,7 @@
                                       data-nilairahmatanlilalamin="<?= $el['nilai_rahmatan_lil_alamin'] ?>"
                                       data-subnilai="<?=$el['sub_nilai']?> "
                                       style="text-decoration:none"
+                                      data-toggle="modal" data-target="#modal-capaian"
                                       >
                                       <i class="icon-note"></i> Edit
                                     </button>  
@@ -288,12 +202,138 @@
             </div>
        </div> 
     </div>
-<!-- Tabel -->
-    <div class="content">
-      <div class="page-inner">
+    
+   
+</div>
 
-      </div> 
+<!-- Modal -->
+<div class="modal fade" id="modal-element" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-lg">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="exampleModalLabel">FORM ELEMENT</h1>
+        <button type="button" class="btn-close" data-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <form action="<?= base_url('admin/p5/store/elemen')?>" id='form-elemen' method="post">
+        <div class="modal-body">  
+            <input type="hidden" name="id_element">
+            <div class="card-body">
+              <div class="row">
+                <div class="col-md-6">
+                  <div class="form-group">
+                    <label for="dimensi">DIMENSI</label>
+                    <select name="dimensi" id="" class='form-control'>
+                      <option value=""></option>
+                      <?php foreach($dimensi as $dimen) : ?>
+                        <option value="<?php echo $dimen['id_dimensi']?>"> <?= $dimen['kode_dimensi'].' - '.$dimen['dimensi']?></option>
+                      <?php endforeach ?>
+                    </select>
+                  </div>
+                  <div class="form-group">
+                    <label for="dimensi">Kode</label>
+                    <input type="text" name="kode_element" class='form-control'>
+                  </div>
+                  <div class="form-group">
+                    <label for="dimensi">Elemen</label>
+                    <textarea name="desc" id="desc" class='form-control' cols="30" rows="3"></textarea>
+                  </div>
+                  
+                </div>
+                <div class="col-md-6">
+                  <?php if(count($elemen) > 0): ?>
+                  <div class="form-group d-none sub-element-input">
+                    <label for="dimensi">Parent Elemen</label>
+                    <select name="id_parent_element" id="" class='form-control'>
+                      <option value="null"></option>
+                      <?php foreach($elemen as $el): ?>
+                        <?php if($el['id_parent_element'] == null): ?>
+                          <option value="<?= $el['id_element']?>"><?php echo $el['kode_element'].' '. $el['desc'] ?></option>
+                        <?php endif ?>
+                      <?php endforeach ?>
+                    </select>
+                  </div>
+                  <?php endif ?>
+                  <div class="form-group d-none sub-element-input">
+                    <label for="dimensi">Nilai Rahmatan Lil Alamin</label>
+                    <input type="text" name="nilai_rahmatan_lil_alamin" class='form-control'>
+                  </div>
+                  <div class="form-group d-none sub-element-input">
+                    <label for="dimensi">Sub Nilai</label>
+                    <input type="text" name="sub_nilai" class='form-control'>
+                  </div>
+
+                </div>
+              </div>
+            </div> 
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+          <button type="submit" class="btn btn-primary">Save changes</button>
+        </div>
+      </form>
     </div>
+  </div>
+</div>
+
+
+<!-- modal capaian -->
+<div class="modal fade" id="modal-capaian" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-lg">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="exampleModalLabel">FORM ELEMENT</h1>
+        <button type="button" class="btn-close" data-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <form action="<?= base_url('admin/p5/store/capaian')?>" method="post">
+      <div class="modal-body">  
+          <input type="hidden" name="id_capaian">
+          <div class="row">
+            <div class="col-md-6">
+              <div class="form-group">
+                <label for="dimensi">Kode Capaian</label>
+                <input type="text" readonly name="kode_capaian" class='form-control'>
+              </div>
+              
+              
+              <div class="form-group">
+                <label for="dimensi">Elemen</label>
+                <select name="id_parent_element" id="parent-element-capaian" class='form-control'>
+                  <option value="null"></option>
+                  <?php foreach($elemen as $el): ?>
+                    <?php if($el['id_parent_element']): ?>
+                      <option data-kodeelement='<?php echo $el['kode_element'] ?>' value="<?= $el['id_element']?>"><?php echo $el['kode_element'].' '. $el['desc'] ?></option>
+                    <?php endif ?>
+                  <?php endforeach ?>
+                </select>
+              </div>
+              <div class="form-group">
+                <label for="dimensi">Capaian</label>
+                <textarea name="desc" id="" class='form-control' cols="30" rows="10">
+                  
+                </textarea>
+              </div>
+            </div>
+            <div class="col-md-6">
+              
+              <div class="form-group">
+                <label for="dimensi">Nilai Rahmatan Lil Alamin</label>
+                <input type="text" name="nilai_rahmatan_lil_alamin" class='form-control'>
+              </div>
+              <div class="form-group">
+                <label for="dimensi">Sub Nilai</label>
+                <input type="text" name="sub_nilai" class='form-control'>
+              </div>
+
+            </div>
+          </div>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+          <button type="submit" class="btn btn-primary">Save changes</button>
+        </div>
+      </form>
+    </div>
+  </div>
 </div>
 <script src="<?= base_url('assets/')?>assets/js/core/jquery.3.2.1.min.js"></script>
 <script>
@@ -323,9 +363,16 @@
     $('input[name=kode_capaian]').val($(this).find(':selected').attr('data-kodeelement'))
   })
 
-  $('select[name=id_parent_element]').on('change',function(e){
-    showInputSubElemen(e.target.value)
+  $('.action-element').click(function(){
+    showInputSubElemen(false)
   })
+  $('.action-sub-element').click(function(){
+    showInputSubElemen(true)
+  })
+
+  // $('select[name=id_parent_element]').on('change',function(e){
+  //   showInputSubElemen(e.target.value)
+  // })
 
   function showInputSubElemen(val){
     if(val){
