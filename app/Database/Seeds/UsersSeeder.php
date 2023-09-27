15fe6341249607a7cd84f17def7af7e4f5248b51
Maulal Ardi Atqo <maulalardiatqo123@gmail.com>
2023-09-27 23:42:31 +0700

    seeders users

--- app/Database/Seeds/UsersSeeder.php
+++ app/Database/Seeds/UsersSeeder.php
@@ -10,10 +10,73 @@ class UsersSeeder extends Seeder
     {
         //
         $data = [
-            'name' => 'Super Admin',
-            'username' => 'admin',
-            'password' => password_hash('12345', PASSWORD_DEFAULT),
-            'role_id' => 1,
+            [
+                'name' => 'Super Admin',
+                'username' => 'admin',
+                'password' => password_hash('12345', PASSWORD_DEFAULT),
+                'role_id' => 1,
+            ],
+            [
+                'name' => 'Super Admin',
+                'username' => 'admin1Mts1',
+                'password' => password_hash('MtsN1@Tegal', PASSWORD_DEFAULT),
+                'role_id' => 1,
+            ],
+            [
+                'name' => 'Super Admin',
+                'username' => 'admin2Mts1',
+                'password' => password_hash('MtsN1@Tegal', PASSWORD_DEFAULT),
+                'role_id' => 1,
+            ],
+            [
+                'name' => 'Super Admin',
+                'username' => 'admin3Mts1',
+                'password' => password_hash('MtsN1@Tegal', PASSWORD_DEFAULT),
+                'role_id' => 1,
+            ],
+            [
+                'name' => 'Super Admin',
+                'username' => 'admin4Mts1',
+                'password' => password_hash('MtsN1@Tegal', PASSWORD_DEFAULT),
+                'role_id' => 1,
+            ],
+            [
+                'name' => 'Super Admin',
+                'username' => 'admin5Mts1',
+                'password' => password_hash('MtsN1@Tegal', PASSWORD_DEFAULT),
+                'role_id' => 1,
+            ],
+            [
+                'name' => 'Super Admin',
+                'username' => 'admin6Mts1',
+                'password' => password_hash('MtsN1@Tegal', PASSWORD_DEFAULT),
+                'role_id' => 1,
+            ],
+            [
+                'name' => 'Super Admin',
+                'username' => 'admin7Mts1',
+                'password' => password_hash('MtsN1@Tegal', PASSWORD_DEFAULT),
+                'role_id' => 1,
+            ],
+            [
+                'name' => 'Super Admin',
+                'username' => 'admin8Mts1',
+                'password' => password_hash('MtsN1@Tegal', PASSWORD_DEFAULT),
+                'role_id' => 1,
+            ],
+            [
+                'name' => 'Super Admin',
+                'username' => 'admin9Mts1',
+                'password' => password_hash('MtsN1@Tegal', PASSWORD_DEFAULT),
+                'role_id' => 1,
+            ],
+            [
+                'name' => 'Super Admin',
+                'username' => 'admin10Mts1',
+                'password' => password_hash('MtsN1@Tegal', PASSWORD_DEFAULT),
+                'role_id' => 1,
+            ],
+           
         ];
         print_r($data);
         $this->db->table('users')->insert($data);
