6f6b31781c7ce7c91c2189e2b9741056877b631e
Maulal Ardi Atqo <maulalardiatqo123@gmail.com>
2023-10-04 19:44:19 +0700

    siswa

--- app/Controllers/Siswa/SiswaController.php
+++ app/Controllers/Siswa/SiswaController.php
@@ -1,21 +1,29 @@
 <?php
 
 namespace App\Controllers\Siswa;
-use App\Models\Siswa\SiswaModel;
+use App\Models\Admin\SiswaModel;
+use App\Models\Admin\KelasModel;
 use App\Controllers\BaseController;
 
 class SiswaController extends BaseController
 {
     protected $siswaModel;
+    protected $kelasModel;
     public function __construct()
     {
         $this->siswaModel = new SiswaModel();
+        $this->kelasModel = new KelasModel();
         
     }
     public function index()
     {
         $session = session();
         $name = $session->get('name');
+        $nism = $session->get('username');
+        $siswa = $this->siswaModel->where('nism', $nism)->first();
+        $idKelas = $siswa['kelas'];
+        $kelas = $this->kelasModel->where('id_kelas', $idKelas)->first();
+        $data['kelas'] = $kelas['tingkat'] . " " . $kelas['nama_kelas'];
         $data['name'] = $name;
         echo view('admin/template_admin/header');
         echo view('admin/template_admin/sidebar_siswa', $data);
