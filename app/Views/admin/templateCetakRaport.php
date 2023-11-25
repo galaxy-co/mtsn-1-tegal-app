44b587a909a8a9d80ea7980a8e8b8998e8daabbf
Maulal Ardi Atqo <maulalardiatqo123@gmail.com>
2023-11-25 07:44:43 +0700

    logo rapot

--- app/Views/admin/templateCetakRaport.php
+++ app/Views/admin/templateCetakRaport.php
@@ -113,7 +113,7 @@
         $data = file_get_contents($path);
         $base64 = 'data:image/' . $type . ';base64,' . base64_encode($data);
 
-        $path2 = 'assets/assets/img/logomtsraport.png';
+        $path2 = 'assets/assets/img/logorapot.png';
         $type2 = pathinfo($path2, PATHINFO_EXTENSION);
         $data2 = file_get_contents($path2);
         $base642 = 'data:image/' . $type2 . ';base64,' . base64_encode($data2);
